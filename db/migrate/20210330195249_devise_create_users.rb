0f7f33737ae678536274f6e292303eda1cffa1e0
Oever González <software@notengobattery.com>
2021-03-30 14:23:51 -0600

    Update devise user model to fit the project's needs

--- db/migrate/20210330195249_devise_create_users.rb
+++ db/migrate/20210330195249_devise_create_users.rb
@@ -8,18 +8,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+       t.integer  :sign_in_count, default: 0, null: false
+       t.datetime :current_sign_in_at
+       t.datetime :last_sign_in_at
+       t.string   :current_sign_in_ip
+       t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -32,7 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :display
+      ## Extra and useful user information to display in page
+      t.string :display # This is like twitter's user name
 
       t.timestamps null: false
     end
