347f63f0a3ad52fdc510552ecef4731fdcd0d95b
Oever González <software@notengobattery.com>
2021-03-31 15:05:42 -0600

    Remove unused column and unique index from User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_203944) do
+ActiveRecord::Schema.define(version: 2021_03_31_210314) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2021_03_31_203944) do
     t.string "display", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["display"], name: "index_users_on_display", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
