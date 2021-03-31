d4d61bc775c136afe26f2f4df175abe3bf9d09f8
Oever González <software@notengobattery.com>
2021-03-31 14:57:52 -0600

    Fix incorrect logic in `paragraphs_with_length`

--- spec/factories/faker_constraint.rb
+++ spec/factories/faker_constraint.rb
@@ -14,10 +14,11 @@ end
 
 def paragraphs_with_length(arg = {})
   arg[:supplemental] ||= true
+  lorem = ""
   loop do
-    lorem = Faker::Lorem.paragraph(sentence_count: arg[:s_cnt],
-                                   random_sentences_to_add: arg[:s_add],
-                                   supplemental: true)
+    lorem += Faker::Lorem.paragraph(sentence_count: arg[:s_cnt],
+                                    random_sentences_to_add: arg[:s_add],
+                                    supplemental: true)
     return lorem if _length_tester(lorem, arg)
 
     lorem += "\n\n"
