336aaabed2492e6ddbdecaa04045e0aba2cc8ed8
Oever González <software@notengobattery.com>
2021-03-31 14:35:44 -0600

    Add validation tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :recoverable, :confirmable, :lockable, :timeoutable and :omniauthable
+  DISPLAY_MIN = 6
+  DISPLAY_MAX = 15
+
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
+require "shared_rutines"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Most of the code for the user is already tested by Devise
+  context "validations" do
+    subject { FactoryBot.build(:user) }
+
+    it { is_expected.to validate_presence_of(:display) }
+    it { is_expected.to validate_length_of(:display).is_at_least(User::DISPLAY_MIN).is_at_most(User::DISPLAY_MAX) }
+  end
 end
