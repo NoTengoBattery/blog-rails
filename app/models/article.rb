fe775abb6fbd18109a9d2c542a51c71c20dc2e3f
Oever González <software@notengobattery.com>
2021-03-31 14:30:42 -0600

    Add validations for the Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,15 @@
 class Article < ApplicationRecord
-  # A set of constants in the model to avoid hard-coding them
-  TITLE_MAX = 60
-  TITLE_MIN = 5
+  # A set of constants in the model to avoid hard-coding them in the seeds and tests
   CONTENT_MIN = 75
   IMG_ALT_MIN = 10
+  TITLE_MAX = 60
+  TITLE_MIN = 5
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :image_caption, presence: true
+  validates :title, length: { minimum: Article::TITLE_MIN, maximum: Article::TITLE_MAX }
+  validates :content, length: { minimum: Article::CONTENT_MIN }
+  validates :image_caption, length: { minimum: Article::IMG_ALT_MIN }
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,9 @@
+require_relative "faker_constraint"
+
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    image_caption { "MyString" }
+    title { sentence_with_length(w_cnt: 10, w_add: 15, min_l: Article::TITLE_MIN, max_l: Article::TITLE_MAX) }
+    content { paragraphs_with_length(s_cnt: 15, s_add: 45, min_l: Article::CONTENT_MIN) }
+    image_caption { sentence_with_length(w_cnt: 5, w_add: 10, min_l: Article::IMG_ALT_MIN) }
   end
 end
