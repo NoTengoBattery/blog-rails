e25c8629d68214157d4d2259c867bbc60cfc3e7f
Oever González <software@notengobattery.com>
2021-03-31 14:47:11 -0600

    Add missing index for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_203738) do
+ActiveRecord::Schema.define(version: 2021_03_31_203944) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_03_31_203738) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
+    t.index ["display"], name: "index_users_on_display", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
