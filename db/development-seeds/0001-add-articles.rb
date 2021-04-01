ae146eef8364f86f2af18f1eb11981c84d137502
Oever González <software@notengobattery.com>
2021-04-01 16:37:54 -0600

    Add seed for generating mock articles

---
+++ db/development-seeds/0001-add-articles.rb
@@ -0,0 +1,13 @@
+progressbar = ProgressBar.create
+progressbar.total = 250
+
+until progressbar.progress >= progressbar.total
+  begin
+    FactoryBot.create(:article)
+    progressbar.increment
+  rescue ActiveRecord::RecordInvalid
+    puts "\n#{$!}, skipping..."
+  end
+end
+
+puts "Created a total of #{progressbar.progress} records"
