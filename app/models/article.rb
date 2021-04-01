0ae6ab2a6a8334231664412de770b09495e7c906
Oever González <software@notengobattery.com>
2021-03-31 22:27:39 -0600

    Implement model and controller code for image attachments

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,6 +13,22 @@ class Article < ApplicationRecord
   validates :title, length: { minimum: Article::TITLE_MIN, maximum: Article::TITLE_MAX }
   validates :content, length: { minimum: Article::CONTENT_MIN }
   validates :image_caption, length: { minimum: Article::IMG_ALT_MIN }
+  validate :acceptable_image
 
   belongs_to :author, class_name: :User, foreign_key: :user_id, inverse_of: :articles
+  has_one_attached :image
+
+  def acceptable_image
+    unless image.attached?
+      errors.add(:image, I18n.t("errors.messages.blank"))
+      return
+    end
+
+    errors.add(:image, I18n.t("errors.messages.oversize", size: "5 MB")) unless image.byte_size <= 5.megabyte
+
+    acceptable_types = ["image/jpeg", "image/png", "image/tiff"]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, I18n.t("errors.messages.format", formats: "JPG, PNG, TIFF"))
+    end
+  end
 end
