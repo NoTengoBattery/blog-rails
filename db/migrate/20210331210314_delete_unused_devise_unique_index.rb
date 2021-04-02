45335adc339a8044552f40cd64058654d8578144
Oever González <software@notengobattery.com>
2021-04-02 15:27:46 -0600

    Remove columns from the migration
    Sadly, the server is rejecting the Devise migration...

--- db/migrate/20210331210314_delete_unused_devise_unique_index.rb
+++
@@ -1,7 +0,0 @@
-class DeleteUnusedDeviseUniqueIndex < ActiveRecord::Migration[6.1]
-  def change
-    # The issue here is that I forgot to remove this index when disabling this Devise module.
-    remove_index :users, :reset_password_token
-    remove_column :users, :reset_password_token
-  end
-end
