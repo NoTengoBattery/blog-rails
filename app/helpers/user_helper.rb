c98bce38c7a608d53374210c26af6e211129a4c2
Oever González <software@notengobattery.com>
2021-04-01 18:44:40 -0600

    General fixes and adjustments to the views

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,18 +1,20 @@
 module UserHelper
   def sign_up_or_profile(params = {})
+    params[:method] = :get
     if signed_in?
-      link_to current_user.display, home_path, params # TODO
+      button_to current_user.display, user_path(current_user), params # TODO
     else
-      link_to I18n.t("devise.shared.links.sign_up"), new_user_registration_path, params
+      button_to I18n.t("devise.shared.links.sign_up"), new_user_registration_path, params
     end
   end
 
   def sign_in_or_out(params = {})
+    params[:method] = :get
     if signed_in?
       params[:method] = :delete
-      link_to I18n.t("actions.sign_out"), destroy_user_session_path, params
+      button_to I18n.t("actions.sign_out"), destroy_user_session_path, params
     else
-      link_to I18n.t("devise.shared.links.sign_in"), user_session_path, params
+      button_to I18n.t("devise.shared.links.sign_in"), user_session_path, params
     end
   end
 
